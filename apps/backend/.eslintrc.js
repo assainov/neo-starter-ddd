dddec72d5e60fec3eaa4d5499cb3ab7957f8e8f5
assainov <iliyas.assainov@gmail.com>
2024-11-05 10:34:28 +0400

    Centralize eslint config in the package

--- apps/backend/.eslintrc.js
+++ apps/backend/.eslintrc.js
@@ -1,48 +1,12 @@
 /* eslint-env node */
 module.exports = {
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/strict-type-checked',
-  ],
+  root: true,
+  extends: [ '@neo/eslint-config/base.js' ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     projectService: {
-      allowDefaultProject: [ '.eslintrc.js' ],
+      allowDefaultProject: [ '.eslintrc.js', 'base.js' ],
     },
     tsconfigRootDir: __dirname
   },
-  plugins: [ '@typescript-eslint', 'import', 'unused-imports' ],
-  root: true,
-  ignorePatterns: [ '**/dist/**/*.js' ],
-  rules: {
-    semi: [ 'error', 'always' ],
-    quotes: [ 'error', 'single' ],
-    'indent': [ 'error', 2 ],
-    'no-multiple-empty-lines': [ 'error', { 'max': 1, 'maxEOF': 0 } ],
-    'no-multi-spaces': 'error',
-    'arrow-body-style': 'warn',
-    'prefer-arrow-callback': [
-      'warn',
-      {
-        allowNamedFunctions: true,
-      },
-    ],
-    'no-trailing-spaces': [ 'error', { 'ignoreComments': true } ],
-    'no-unused-vars': 'off',
-    '@typescript-eslint/no-unused-vars': 'off',
-    'unused-imports/no-unused-imports': 'error',
-    'unused-imports/no-unused-vars': [
-      'warn',
-      {
-        'vars': 'all',
-        'varsIgnorePattern': '^_',
-        'args': 'after-used',
-        'argsIgnorePattern': '^_',
-      },
-    ],
-    'import/no-dynamic-require': 'error',
-    'object-curly-spacing': [ 'error', 'always' ],
-    'array-bracket-spacing': [ 'error', 'always' ],
-    '@typescript-eslint/explicit-member-accessibility': 'error'
-  }
 };
---
+++ packages/eslint-config/base.js
@@ -0,0 +1,46 @@
+const { resolve } = require('node:path');
+ 
+const project = resolve(process.cwd(), 'tsconfig.json');
+
+module.exports = {
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/strict-type-checked',
+  ],
+  plugins: [ '@typescript-eslint', 'import', 'unused-imports' ],
+  ignorePatterns: [
+    '**/dist/**/*.js',
+    '**/node_modules/**/*.js',
+  ],
+  rules: {
+    semi: [ 'error', 'always' ],
+    quotes: [ 'error', 'single' ],
+    'indent': [ 'error', 2 ],
+    'no-multiple-empty-lines': [ 'error', { 'max': 1, 'maxEOF': 0 } ],
+    'no-multi-spaces': 'error',
+    'arrow-body-style': 'warn',
+    'prefer-arrow-callback': [
+      'warn',
+      {
+        allowNamedFunctions: true,
+      },
+    ],
+    'no-trailing-spaces': [ 'error', { 'ignoreComments': true } ],
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
+    'unused-imports/no-unused-imports': 'error',
+    'unused-imports/no-unused-vars': [
+      'warn',
+      {
+        'vars': 'all',
+        'varsIgnorePattern': '^_',
+        'args': 'after-used',
+        'argsIgnorePattern': '^_',
+      },
+    ],
+    'import/no-dynamic-require': 'error',
+    'object-curly-spacing': [ 'error', 'always' ],
+    'array-bracket-spacing': [ 'error', 'always' ],
+    '@typescript-eslint/explicit-member-accessibility': 'error'
+  }
+};
