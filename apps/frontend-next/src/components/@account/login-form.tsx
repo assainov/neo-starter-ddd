de1ed3477a26069899ac74116649ed1f9d25532c
assainov <iliyas.assainov@gmail.com>
2024-11-12 21:10:07 +0400

    Connect LoginForm and RegisterForm logic

--- apps/frontend-next/src/components/@account/login-form.tsx
+++ apps/frontend-next/src/components/@account/login-form.tsx
@@ -1,3 +1,4 @@
+'use client';
 import Link from 'next/link';
 
 import { Button } from '@/components/ui/button';
@@ -10,49 +11,68 @@ import {
 } from '@/components/ui/card';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
+import { loginUserBodySchema, LoginUserBody } from '@neo/application/user';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { ROUTE } from '@/constants/routes';
 
-export const LoginForm = () => (
-  <Card className="mx-auto max-w-sm">
-    <CardHeader>
-      <CardTitle className="text-2xl">Login</CardTitle>
-      <CardDescription>
-        Enter your email below to login to your account
-      </CardDescription>
-    </CardHeader>
-    <CardContent>
-      <div className="grid gap-4">
-        <div className="grid gap-2">
-          <Label htmlFor="email">Email</Label>
-          <Input
-            id="email"
-            placeholder="m@example.com"
-            required
-            type="email"
-          />
-        </div>
-        <div className="grid gap-2">
-          <div className="flex items-center">
-            <Label htmlFor="password">Password</Label>
-            {/* <Link
+export const LoginForm = () => {
+  const { register, handleSubmit, formState: { errors, isSubmitting } } = useForm<LoginUserBody>({
+    resolver: zodResolver(loginUserBodySchema),
+  });
+
+  const onSubmit: SubmitHandler<LoginUserBody> = async (data) => {
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+    console.log(data);
+  };
+
+  return (
+    <Card className="mx-auto max-w-sm">
+      <CardHeader>
+        <CardTitle className="text-2xl">Login</CardTitle>
+        <CardDescription>
+          Enter your email below to login to your account
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <form
+          className="grid gap-4"
+          onSubmit={handleSubmit(onSubmit)}
+        >
+          <div className="grid gap-2">
+            <Label htmlFor="email">Email</Label>
+            <Input
+              id="email"
+              placeholder="m@example.com"
+              {...register('email')}
+            />
+            {errors.email ? <div className='text-sm text-red-500'>{errors.email.message}</div> : null}
+          </div>
+          <div className="grid gap-2">
+            <div className="flex items-center">
+              <Label htmlFor="password">Password</Label>
+              {/* <Link
               className="ml-auto inline-block text-sm underline"
               href="#"
             >
               Forgot your password?
             </Link> */}
+            </div>
+            <Input
+              id="password"
+              type="password"
+              {...register('password')}
+            />
+            {errors.password ? <div className='text-sm text-red-500'>{errors.password.message}</div> : null}
           </div>
-          <Input
-            id="password"
-            required
-            type="password"
-          />
-        </div>
-        <Button
-          className="w-full"
-          type="submit"
-        >
-          Login
-        </Button>
-        {/* <div className="relative my-4">
+          <Button
+            className="w-full"
+            disabled={isSubmitting}
+            type="submit"
+          >
+            {isSubmitting ? 'Loading...' : 'Login'}
+          </Button>
+          {/* <div className="relative my-4">
           <div className="absolute inset-0 flex items-center">
             <span className="w-full border-t" />
           </div>
@@ -69,16 +89,17 @@ export const LoginForm = () => (
           <GoogleIcon />
           Google
         </Button> */}
-      </div>
-      <div className="mt-8 text-center text-sm">
-        Don&apos;t have an account?{' '}
-        <Link
-          className="underline"
-          href="/sign-up"
-        >
-          Sign up
-        </Link>
-      </div>
-    </CardContent>
-  </Card>
-);
+        </form>
+        <div className="mt-8 text-center text-sm">
+          Don&apos;t have an account?{' '}
+          <Link
+            className="underline"
+            href={ROUTE.AccountRegister}
+          >
+            Register
+          </Link>
+        </div>
+      </CardContent>
+    </Card>
+  );
+};
--- apps/frontend-next/src/components/@account/register-form.tsx
+++ apps/frontend-next/src/components/@account/register-form.tsx
@@ -1,3 +1,4 @@
+'use client';
 import Link from 'next/link';
 
 import { Button } from '@/components/ui/button';
@@ -10,45 +11,82 @@ import {
 } from '@/components/ui/card';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
+import { ROUTE } from '@/constants/routes';
+import { RegisterUserBody, registerUserBodySchema } from '@neo/application/user';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { SubmitHandler, useForm } from 'react-hook-form';
 
-export const RegisterForm = () => (
-  <Card className="mx-auto max-w-sm">
-    <CardHeader>
-      <CardTitle className="text-2xl">
-        Create an account
-      </CardTitle>
-      <CardDescription>
-        Enter your email to start creating your account
-      </CardDescription>
-    </CardHeader>
-    <CardContent>
-      <div className="grid gap-4">
-        <div className="grid gap-2">
-          <Label htmlFor="email">Email</Label>
-          <Input
-            id="email"
-            placeholder="m@example.com"
-            required
-            type="email"
-          />
-        </div>
-        <div className="grid gap-2">
-          <div className="flex items-center">
-            <Label htmlFor="password">Password</Label>
-          </div>
-          <Input
-            id="password"
-            required
-            type="password"
-          />
-        </div>
-        <Button
-          className="w-full"
-          type="submit"
+export const RegisterForm = () => {
+  const { register, handleSubmit, formState: { errors, isSubmitting } } = useForm<RegisterUserBody>({
+    resolver: zodResolver(registerUserBodySchema),
+  });
+
+  const onSubmit: SubmitHandler<RegisterUserBody> = async (data) => {
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+    console.log(data);
+  };
+
+  return (
+    <Card className="mx-auto max-w-sm">
+      <CardHeader>
+        <CardTitle className="text-2xl">
+          Create an account
+        </CardTitle>
+        <CardDescription>
+          Enter your email to start creating your account
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <form
+          className="grid gap-4"
+          onSubmit={handleSubmit(onSubmit)}
         >
-          Start
-        </Button>
-        {/* <div className="relative my-4">
+          <div className="grid gap-2">
+            <Label htmlFor="firstName">First name</Label>
+            <Input
+              id="firstName"
+              placeholder="John"
+              {...register('firstName')}
+            />
+            {errors.firstName ? <div className='text-sm text-red-500'>{errors.firstName.message}</div> : null}
+          </div>
+          <div className="grid gap-2">
+            <Label htmlFor="lastName">Last name</Label>
+            <Input
+              id="lastName"
+              placeholder="Doe"
+              {...register('lastName')}
+            />
+            {errors.lastName ? <div className='text-sm text-red-500'>{errors.lastName.message}</div> : null}
+          </div>
+          <div className="grid gap-2">
+            <Label htmlFor="email">Email</Label>
+            <Input
+              id="email"
+              placeholder="m@example.com"
+              {...register('email')}
+            />
+            {errors.email ? <div className='text-sm text-red-500'>{errors.email.message}</div> : null}
+          </div>
+          <div className="grid gap-2">
+            <div className="flex items-center">
+              <Label htmlFor="password">Password</Label>
+            </div>
+            <Input
+              id="password"
+              type="password"
+              {...register('password')}
+            />
+            {errors.password ? <div className='text-sm text-red-500'>{errors.password.message}</div> : null}
+          </div>
+          <Button
+            className="w-full"
+            disabled={isSubmitting}
+            type="submit"
+          >
+            {isSubmitting ? 'Loading...' : 'Create account'}
+          </Button>
+          {/* <div className="relative my-4">
           <div className="absolute inset-0 flex items-center">
             <span className="w-full border-t" />
           </div>
@@ -64,16 +102,17 @@ export const RegisterForm = () => (
         >
           Google
         </Button> */}
-      </div>
-      <div className="mt-8 text-center text-sm">
-        Have an account already?{' '}
-        <Link
-          className="underline"
-          href="/sign-in"
-        >
-          Log in
-        </Link>
-      </div>
-    </CardContent>
-  </Card>
-);
+        </form>
+        <div className="mt-8 text-center text-sm">
+          Have an account already?{' '}
+          <Link
+            className="underline"
+            href={ROUTE.AccountLogin}
+          >
+            Log in
+          </Link>
+        </div>
+      </CardContent>
+    </Card>
+  );
+};
